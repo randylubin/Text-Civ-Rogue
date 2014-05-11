42594d331b0f2a9697d2c9e910dfc8adf1f91be0
Randy Lubin <randylubin@gmail.com>
2014-05-11 14:47:15 -0700

    interrupts and cheating added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,70 +10,45 @@ function startingData(){
 	this.population = 30
 	this.religion = 'Animism'
 	this.government = 'Egalitarianism'
+	this.agriculture = 0
 	this.turnsStationary = 0
 	this.carryingCapacity = 100
 }
 
-var utils = {
-	landAdjective: [
-		"fertile",
-		"desolate",
-		"pleasant",
-		"lush",
-		"barren",
-		"temperate",
-		"bountiful",
-		"abundant",
-		"safe",
-		"glorious",
-		"breathtaking",
-		"gorgeous"
-	],
-
-	landType: [
-		"valley",
-		"vale",
-		"plain",
-		"canyon",
-		"coast",
-		"isthmus",
-		"expanse",
-		"land",
-		"region",
-		"place"
-	],
-
-	pickRandomFromList: function(list){
-		return list[Math.floor(Math.random()*list.length)];
-	},
-	
-	randomLandDescription: function(){
-		var adjective = utils.pickRandomFromList(utils.landAdjective);
-		var type = utils.pickRandomFromList(utils.landType);
-
-		return adjective + " " + type;
-
-	}
+function startingAlerts(){
+	this.sacrifices = 0;
 }
 
-angular.module('myApp.controllers', [])
-  .controller('GameCtrl', ['$scope', function($scope) {
+angular.module('myApp.controllers', ['ngSanitize'])
+  .controller('GameCtrl', ['$scope', 'utils', 'gameData', function($scope, utils, gameData) {
   	$scope.gui = {};
+  	$scope.gameData = gameData;
   	$scope.gameData = new startingData;
   	$scope.gameData.land = utils.randomLandDescription();
+
+  	$scope.alerts = {};
+  	$scope.alerts.alertData = new startingAlerts();
+
+  	$scope.alerts.messages = function(){
+		if ($scope.alerts.alertData.sacrifices >= 2){
+			alert("It's a volcano... no amount of sacrifice will save you.");
+			$scope.alerts.alertData.sacrifices = 0;
+		}
+	};
 
 	$scope.scenes = {}
 	$scope.scenes.list = {
 		intro: {
-			message: "The mountain spews fire. The mountain spirit must be angry. Kinsman have always hunted and gathered here. They turn to you, " + $scope.gameData.leader + ", for guidance.",
 			actionPrompt: "Abandon homeland or make a sacrifice to the mountain?",
 			choices: [
 		  		{text: "Flee", nextScene: 'flee', data: {}},
 		  		{text: "Sacrifice", nextScene: 'sacrifice', data: {}}
 		  	],
 		  	script: function(data){
+		  		$scope.gui.message = "The mountain spews fire. The mountain spirit must be angry. Kinsman have always hunted and gathered here. They turn to you, " + $scope.gameData.leader + ", for guidance."
 		  		$scope.gameData = new startingData;
-		  	}
+		  	},
+		  	interceptable: false
 		},
 		flee: {
 			message: "You lead your band away from the mountain. The mountain explodes. The band heads east, away from the ash. They stop in a valley, lush with flora and fauna.",
@@ -84,8 +59,9 @@ angular.module('myApp.controllers', [])
 		  	],
 		  	script: function(data){
 		  		$scope.gameData.population += Math.round((Math.random()-.5)*2);
-
-		  	}
+		  		$scope.gui.message = this.message;
+		  	},
+		  	interceptable: false
 		},
 		firstMove: {
 			message: "", // done in script
@@ -99,8 +75,9 @@ angular.module('myApp.controllers', [])
 		  		$scope.scenes.list.firstMove.message =  "Your band moves onward, foraging as you go. You reach a "+ $scope.gameData.land +".";
 		  		$scope.gameData.population += Math.round((Math.random()-.5)*3);
 		  		$scope.gameData.turnsStationary = 0;
-
-		  	}
+		  		$scope.gui.message = this.message;
+		  	},
+		  	interceptable: false
 		},
 		move: {
 			message: "", //done in script
@@ -114,8 +91,10 @@ angular.module('myApp.controllers', [])
 		  		$scope.scenes.list.move.message =  "Your band moves onward, foraging as you go. You reach a "+ $scope.gameData.land +".";
 		  		$scope.gameData.population += Math.round((Math.random()-.5)*3);
 		  		$scope.gameData.turnsStationary = 0;
+		  		$scope.gui.message = this.message;
 
-		  	}
+		  	},
+		  	interceptable: false
 		},
 		firstSettle: {
 			message: "Your people are content in their new land. Food is abundant and the weather is fair. Children are born who will never know life at the foot of the angry mountain. After several seasons you scrape your toe and die from an infection. No matter - your kinsman are wise and can thrive without you.",
@@ -128,35 +107,37 @@ angular.module('myApp.controllers', [])
 		  		$scope.gameData.population += Math.round(Math.random()*5)+2;
 		  		$scope.gameData.generation += 1;
 		  		$scope.gameData.turnsStationary = 1;
-		  	}
+		  		$scope.gui.message = this.message;
+		  	},
+		  	interceptable: false
 		},
 		stayAndGrow: {
 			message: "The "+ $scope.gameData.land +" becomes home. One of your key concerns is population growth.",
 			actionPrompt: "What does the future hold for your band?",
-			choices: [
-		  		{text: "Stablize Population", nextScene: 'growth', data: ['stable']},
-		  		{text: "High Growth", nextScene: 'growth', data: ['high']},
-		  		{text: "Shrink Band", nextScene: 'growth', data: ['shrink']},
-		  		{text: "Move on", nextScene: 'move', data: {}}
-		  	],
+			choices: utils.growthChoices,
 		  	script: function(data){
-		  		$scope.gameData.turnsStationary += 1;
-		  	}
+		  		$scope.gameData.turnsStationary = 1;
+		  		$scope.gui.message = this.message;
+		  	},
+		  	interceptable: false
 		},
 		growth: {
 			message: "",
 			actionPrompt: "",
-			choices: [
-		  		{text: "Restart", nextScene: 'TODO', data: {}},
-		  		{text: "Restart", nextScene: 'TODO', data: {}}
-		  	],
+			choices: utils.growthChoices,
 		  	script: function(data){
 		  		$scope.gameData.generation += 1;
+		  		$scope.gameData.turnsStationary += 1;
 		  		var growthReturn = $scope.growthProcessing(data, $scope.gameData.population, $scope.gameData.carryingCapacity)
-		  		$scope.scenes.list.growth.message = growthReturn.message;
-		  		$scope.scenes.list.growth.actionPrompt = growthReturn.actionPrompt;
-		  		$scope.scenes.list.growth.choices = growthReturn.choices;
-		  	}
+		  		this.message = growthReturn.message;
+		  		this.actionPrompt = growthReturn.actionPrompt;
+		  		this.choices = growthReturn.choices;
+
+
+		  		$scope.gui.message = this.message;
+		  		$scope.gui.actionPrompt = this.actionPrompt
+		  	},
+		  	interceptable: true
 		},
 		sacrifice: {
 			message: "You tell your kinsmen to prepare a giant pyre for the sacrifice. They start building. They gather food for a feast.",
@@ -167,7 +148,10 @@ angular.module('myApp.controllers', [])
 		  		{text: "An Elder", nextScene: 'pyroclastic', data: {}}
 		  	],
 		  	script: function(data){
-		  	}
+		  		$scope.gui.message = this.message;
+
+		  	},
+		  	interceptable: false
 		},
 		pyroclastic: {
 			message: "The pyre illuminates the night sky. As the ceremony begins, you are engulfed by a pyroclastic flow. Your band dies a swift and painless death.",
@@ -177,7 +161,10 @@ angular.module('myApp.controllers', [])
 		  	],
 		  	script: function(data){
 		  		$scope.gameData.population = 0;
-		  	}
+		  		$scope.alerts.alertData.sacrifices += 1;
+		  		$scope.gui.message = this.message;
+		  	},
+		  	interceptable: false
 		},
 		TODO: {
 			message: "",
@@ -187,16 +174,32 @@ angular.module('myApp.controllers', [])
 		  		{text: "Restart", nextScene: 'intro', data: {}}
 		  	],
 		  	script: function(data){
+		  		$scope.gui.message = this.message;
 
-		  	}
+		  	},
+		  	interceptable: false
 		}
 	};
-	$scope.scenes.goToScene = function(sceneName, text, data) {
+	$scope.scenes.goToScene = function(sceneName, text, interceptable, data) {
 		$scope.gameData.scene = sceneName;
+
 		console.log('Scene: ' + sceneName + " Text: " + text);
-		$scope.gui = $scope.scenes.list[$scope.gameData.scene]
-		$scope.gui.script(data);
+		$scope.gui.actionPrompt = $scope.scenes.list[$scope.gameData.scene].actionPrompt
+		$scope.gui.choices = $scope.scenes.list[$scope.gameData.scene].choices
+		$scope.scenes.list[$scope.gameData.scene].script(data);
+
+		if ($scope.scenes.list[$scope.gameData.scene].interceptable){
+			var interceptReturn = $scope.interceptEvaluation(sceneName);
+	  		if (interceptReturn.intercepted == true){
+	  			$scope.gui.message += interceptReturn.message;
+	  			$scope.gui.actionPrompt = interceptReturn.actionPrompt;
+	  			$scope.gui.choices = interceptReturn.choices;
+	  			
+	  		}
+		}
+		$scope.alerts.messages();
 	};
+
 	$scope.growthProcessing = function(data, population, carryingCapacity){
 		var message;
 
@@ -208,6 +211,7 @@ angular.module('myApp.controllers', [])
 				break
 			case 'stable':
 				message = "The band is stable."
+				population += Math.round((Math.random()-.5)*2);
 				break
 			case 'shrink':
 				population -= Math.floor(population * 0.2)
@@ -216,45 +220,66 @@ angular.module('myApp.controllers', [])
 		}
 
 		// carrying capacity
-		
+		message += "<br /><br />"
+
 		var populationResourceRatio = population / carryingCapacity
 
 		if (populationResourceRatio < 0.4){
-			message += " This land easily supports your band. Your people spend most of their time in leisure."
+			message += "This land easily supports your band. Your people spend most of their time in leisure."
 		}else if (populationResourceRatio < 0.75){
-			message += " There are enough resources here to support your band. Your people spend some time foraging but there is still plenty of leisure."
+			message += "There are enough resources here to support your band. Your people spend some time foraging but there is still plenty of leisure."
 		}else if (populationResourceRatio <= 1.0){
-			message += " There are barely enough resources here to support your band. Your people spend most of their time scrounging for food."
+			message += "There are barely enough resources here to support your band. Your people spend most of their time scrounging for food."
 		}else if (populationResourceRatio > 1.0){
-			message += " This land cannot support your band. You are too many and it is not nearly fertile enough. Your people spend all their time foraging and yet still some starve."
+			message += "This land cannot support your band. You are too many and it is not nearly fertile enough. Your people spend all their time foraging and yet still some starve."
 			population -= Math.floor(Math.random()*(population - carryingCapacity))+5;	
 		}
 
 		var actionPrompt = "What now?"
-		var choices = [
-		  		{text: "Stablize Population", nextScene: 'growth', data: ['stable']},
-		  		{text: "High Growth", nextScene: 'growth', data: ['high']},
-		  		{text: "Shrink Band", nextScene: 'growth', data: ['shrink']},
-		  		{text: "Move on", nextScene: 'move', data: {}}
-		  	]
 
 		var sceneInfo = {
 			message: message,
 			actionPrompt: actionPrompt,
-			choices: choices
+			choices: utils.growthChoices
 		}
 
 		$scope.gameData.population = population
 		return sceneInfo
 	}
 
+	$scope.interceptEvaluation = function(data){	
+
+		var interceptReturn = {
+			intercepted: false
+		}
+
+
+
+		// agriculture
+		if (($scope.gameData.turnsStationary > 1) && !$scope.gameData.agriculture){
+			var compoundingOdds = 0.95;
+			interceptReturn.intercepted = (Math.random() > Math.pow(compoundingOdds,$scope.gameData.turnsStationary))
+			if (interceptReturn.intercepted) {
+				$scope.gameData.agriculture = 1;
+				interceptReturn.message = "<br /><br />You've discovered agriculture!"
+				interceptReturn.actionPrompt = "Wheeeee!";
+				interceptReturn.choices = [
+			  		{text: "Restart", nextScene: 'intro', data: {}},
+			  		{text: "Restart", nextScene: 'intro', data: {}}
+			  	]				
+			}
+		}
+
+		return interceptReturn;
+	}
 
 
 
 
 
-	$scope.gui = $scope.scenes.list[$scope.gameData.scene]
-  	
+	$scope.gui.actionPrompt = $scope.scenes.list[$scope.gameData.scene].actionPrompt
+	$scope.gui.choices = $scope.scenes.list[$scope.gameData.scene].choices
+	$scope.scenes.list[$scope.gameData.scene].script()
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
--- app/js/services.js
+++ app/js/services.js
@@ -2,8 +2,58 @@
 
 /* Services */
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+	service('utils', [function(){
+
+		this.landAdjective = [
+			"fertile",
+			"desolate",
+			"pleasant",
+			"lush",
+			"barren",
+			"temperate",
+			"bountiful",
+			"abundant",
+			"safe",
+			"glorious",
+			"breathtaking",
+			"gorgeous"
+		],
+
+		this.landType = [
+			"valley",
+			"vale",
+			"plain",
+			"canyon",
+			"coast",
+			"isthmus",
+			"expanse",
+			"land",
+			"region",
+			"place"
+		],
+
+		this.growthChoices = [
+	  		{text: "Stablize Population", nextScene: 'growth', data: ['stable']},
+	  		{text: "High Growth", nextScene: 'growth', data: ['high']},
+	  		{text: "Shrink Band", nextScene: 'growth', data: ['shrink']},
+	  		{text: "Move on", nextScene: 'move', data: {}}
+	  	],
+
+		this.pickRandomFromList = function(list){
+			return list[Math.floor(Math.random()*list.length)];
+		},
+		
+		this.randomLandDescription = function(){
+			var adjective = this.pickRandomFromList(this.landAdjective);
+			var type = this.pickRandomFromList(this.landType);
+
+			return adjective + " " + type;
+
+		}
+  	
+	}]).
+	service('gameData', [function(){
+		
+	}])
+
